02fb911ec60905553e3e726b54e266c64c173b43
Terrence Kuiper <github@terrencekuiper.com>
2019-09-25 14:21:20 -0700

    Enabling 'ctr C' to exit game

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = (data) => {
+  if (data === '\x62') {
+    process.stdout.write('\x07');
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.handleUserInput();
+  return stdin;
+};
+
