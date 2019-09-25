48655c8f13eddbaf155b010243ab2b8b0b17bc20
Terrence Kuiper <github@terrencekuiper.com>
2019-09-25 16:28:30 -0700

    Created constants.js and moved some code over to it.

--- client.js
+++ client.js
@@ -1,39 +1,19 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
+
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: '192.168.43.245',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: TDK");
-
-
   });
-  // conn.on('connect', () => {
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 50);
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 100);
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 150);
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 200);
-  //   //conn.write("Move: up");
-  //   //conn.write("Name: TDK");
-  //   console.log("Here");
-
-  // });
-
-
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
